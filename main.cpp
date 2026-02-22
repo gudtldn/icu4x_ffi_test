f209a813f87682bd5a27fe375d8469ba896ce0ef
gudtldn <gudtldn1234@gmail.com>
2026-02-22 14:31:19 +0900

    add: implement ICU4X DecimalFormatter example for Bangla locale

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
+#include <icu4x/DecimalFormatter.hpp>
+#include <icu4x/Logger.hpp>
+
 #include <print>
+
 
 int main()
 {
-    std::println("Hello World!");
+    using namespace icu4x;
+
+    // For basic logging
+    Logger::init_simple_logger();
+
+    // Create a locale object representing Bangla
+    const std::unique_ptr<Locale> locale = Locale::from_string("bn").ok().value();
+
+    std::println("Running test for locale {}", locale->to_string());
+
+    // Create a formatter object with the appropriate settings
+    const std::unique_ptr<DecimalFormatter> formatter =
+        DecimalFormatter::create_with_grouping_strategy(*locale, DecimalGroupingStrategy::Auto).ok().value();
+
+    // Create a decimal representing the number 1,000,007
+    const std::unique_ptr<Decimal> decimal = Decimal::from(1000007);
+
+    // Format it to a string
+    std::string out = formatter->format(*decimal);
+
+    // Report formatted value
+    std::println("Formatted value is {}", out);
+    if (out != "১০,০০,০০৭")
+    {
+        std::println("Output does not match expected output");
+        return 1;
+    }
+
     return 0;
 }
